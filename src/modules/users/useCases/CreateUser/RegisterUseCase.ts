10ee3c9f685de94022e5b15264d893009b6fd2d1
AlbertDev33 <alber725@gmail.com>
2021-03-16 20:51:51 -0300

    feat: ensure that not register with a invalid cpf

--- src/modules/users/useCases/CreateUser/RegisterUseCase.ts
+++ src/modules/users/useCases/CreateUser/RegisterUseCase.ts
@@ -1,16 +1,22 @@
+import { AppError } from '../../../../shared/errors/AppError';
+import { ICpfValidatorProvider } from '../../../../shared/providers/CpfValidator/protocol/ICpfValidatorProvider';
 import { Account } from '../../infra/typeorm/schema/Account';
 import { IRegisterAccountRepository } from '../../repositories/protocol/IRegisterAccountRepository';
 
 interface IRequest {
   name: string;
-  cpf: number;
+  cpf: string;
   cellphone: number;
   score: number;
   negative: boolean;
 }
 
 export class RegisterUseCase {
-  constructor(private registerAccountRepository: IRegisterAccountRepository) {}
+  constructor(
+    private registerAccountRepository: IRegisterAccountRepository,
+
+    private cpfValidatorProvider: ICpfValidatorProvider,
+  ) {}
 
   async execute({
     name,
@@ -19,6 +25,12 @@ export class RegisterUseCase {
     score,
     negative,
   }: IRequest): Promise<Account> {
+    const cpfValidator = this.cpfValidatorProvider.isValid(cpf);
+
+    if (!cpfValidator) {
+      throw new AppError('Invalid Cpf', 400);
+    }
+
     const account = await this.registerAccountRepository.create({
       name,
       cpf,
