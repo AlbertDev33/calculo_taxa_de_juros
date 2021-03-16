555931f80943589daba190d9133b8187ed740ed8
AlbertDev33 <alber725@gmail.com>
2021-03-16 20:52:07 -0300

    test: ensure that not register with a invalid cpf

--- src/modules/users/useCases/CreateUser/RegisterUseCase.spec.ts
+++ src/modules/users/useCases/CreateUser/RegisterUseCase.spec.ts
@@ -1,4 +1,8 @@
 /* eslint-disable max-classes-per-file */
+import { cpf } from 'cpf-cnpj-validator';
+
+import { AppError } from '../../../../shared/errors/AppError';
+import { ICpfValidatorProvider } from '../../../../shared/providers/CpfValidator/protocol/ICpfValidatorProvider';
 import { IRegisterAccountDTO } from '../../dtos/IRegisterAccountDTO';
 import { Account } from '../../infra/typeorm/schema/Account';
 import { IRegisterAccountRepository } from '../../repositories/protocol/IRegisterAccountRepository';
@@ -7,47 +11,85 @@ import { RegisterUseCase } from './RegisterUseCase';
 interface ISutTypes {
   sut: RegisterUseCase;
   registerAccountRepositoryStub: IRegisterAccountRepository;
+  cpfValidatorStub: ICpfValidatorProvider;
 }
+
+const makeCpfValidator = (): ICpfValidatorProvider => {
+  class CpfValidatorStub implements ICpfValidatorProvider {
+    isValid(cpf: string): boolean {
+      return true;
+    }
+  }
+
+  return new CpfValidatorStub();
+};
 
 const makeRegisterAccountRepository = (): IRegisterAccountRepository => {
   class RegisterAccountStub {
     async create(account: IRegisterAccountDTO): Promise<Account> {
-      return new Promise(resolve => resolve(account));
+      return new Promise(resolve => resolve({ ...account, id: 'valid_id' }));
     }
   }
   return new RegisterAccountStub();
 };
 
 const makeSut = (): ISutTypes => {
+  const cpfValidatorStub = makeCpfValidator();
   const registerAccountRepositoryStub = makeRegisterAccountRepository();
-  const sut = new RegisterUseCase(registerAccountRepositoryStub);
+  const sut = new RegisterUseCase(
+    registerAccountRepositoryStub,
+    cpfValidatorStub,
+  );
 
   return {
     sut,
     registerAccountRepositoryStub,
+    cpfValidatorStub,
   };
 };
 
 describe('Create User', () => {
-  it('Sholud be able to register', async () => {
-    const { sut } = makeSut();
+  it('Sholud be able to register with valid cpf', async () => {
+    const { sut, cpfValidatorStub } = makeSut();
 
     const fakeRegister = {
       name: 'valid_name',
-      cpf: 123456,
+      cpf: '123456',
       cellphone: 999999,
       score: 500,
       negative: false,
     };
 
+    cpf.isValid(fakeRegister.cpf);
+
+    const cpfValidator = jest
+      .spyOn(cpfValidatorStub, 'isValid')
+      .mockReturnValueOnce(true);
+
     const register = await sut.execute(fakeRegister);
 
-    expect(register).toEqual({
+    expect(cpfValidator).toHaveBeenCalledWith('123456');
+    expect(register).toHaveProperty('id');
+  });
+
+  it('Sholud not be able to register with invalid cpf', async () => {
+    const { sut, cpfValidatorStub } = makeSut();
+
+    const fakeRegister = {
       name: 'valid_name',
-      cpf: 123456,
+      cpf: '123456',
       cellphone: 999999,
       score: 500,
       negative: false,
-    });
+    };
+
+    const cpfValidator = cpf.isValid(fakeRegister.cpf);
+
+    jest.spyOn(cpfValidatorStub, 'isValid').mockReturnValueOnce(false);
+
+    const register = sut.execute(fakeRegister);
+
+    expect(cpfValidator).toBe(false);
+    await expect(register).rejects.toBeInstanceOf(AppError);
   });
 });
