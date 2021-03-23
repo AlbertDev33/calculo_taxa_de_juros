ca45ed3b799f143ae5eb375acf03ff8aa46072ef
AlbertDev33 <alber725@gmail.com>
2021-03-23 09:31:41 -0300

    refactor: add new routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express';
 
+import { consultSessionRouter } from '../../../../modules/users/routes/consultSession.routes';
+import { loanSimulationRouter } from '../../../../modules/users/routes/loanSimulation.routes';
 import { registerRouter } from '../../../../modules/users/routes/register.routes';
 
 export const router = Router();
 
 router.use('/register', registerRouter);
+router.use('/session', consultSessionRouter);
+router.use('loan', loanSimulationRouter);
