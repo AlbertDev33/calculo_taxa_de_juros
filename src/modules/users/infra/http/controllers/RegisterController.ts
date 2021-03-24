ed0c366338d243965a0cacece6d3250532121e90
AlbertDev33 <alber725@gmail.com>
2021-03-24 11:30:43 -0300

    feat: dependecy inversion of ClassToClass lib

--- src/modules/users/infra/http/controllers/RegisterController.ts
+++ src/modules/users/infra/http/controllers/RegisterController.ts
@@ -1,11 +1,17 @@
+import { ITransformerProvider } from '../../../../../shared/providers/ClassTransformerProvider/model/ITransformerProvider';
 import {
   IRequest,
   IResponse,
 } from '../../../../../shared/providers/ExpressProvider/HttpRequest';
 import { IRegisterUseCase } from '../../../useCases/CreateUser/model/IRegisterUseCase';
+import { Account } from '../../typeorm/schema/Account';
 
 export class RegisterController {
-  constructor(private registerUseCase: IRegisterUseCase) {}
+  constructor(
+    private registerUseCase: IRegisterUseCase,
+
+    private transformerProvider: ITransformerProvider,
+  ) {}
 
   async handle(request: IRequest, response: IResponse): Promise<IResponse> {
     const { name, email, cpf, cellPhone } = request.body;
@@ -17,6 +23,8 @@ export class RegisterController {
       cellPhone,
     });
 
-    return response.status(201).json(register);
+    return response.status(201).json({
+      register: this.transformerProvider.internalTransform<Account>(register),
+    });
   }
 }
