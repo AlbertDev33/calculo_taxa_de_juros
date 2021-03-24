051e055cd6f6370c3a8bc7569978766d0159fa48
AlbertDev33 <alber725@gmail.com>
2021-03-24 09:33:39 -0300

    refactor: environment variables configuration

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,11 +2,10 @@ import { openConnection } from '../../../modules/users/infra/typeorm';
 
 openConnection()
   .then(async () => {
-    const port = 3333;
     const app = (await import('../http/config/app')).default;
 
-    app.listen(port, () => {
-      console.info(`🚀 Executando na porta ${port}`);
+    app.listen(process.env.SERVER_PORT, () => {
+      console.info(`🚀 Executando na porta ${process.env.SERVER_PORT}`);
     });
   })
   .catch(console.error);
