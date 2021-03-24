b4c7a78d8f36e56769c2aeeb046159f2c6849514
AlbertDev33 <alber725@gmail.com>
2021-03-23 22:59:00 -0300

    test: ensure that create an user with valid values

--- src/__test__/register.spec.ts
+++ src/__test__/register.spec.ts
@@ -17,19 +17,45 @@ describe('Register User', () => {
     await disconnect();
   });
 
-  it('Should be able to register a user', async () => {
-    const mockValidationCpf = new CpfValidatorProvider();
-
+  it('Should be able to register a user with valid cpf and unused email', async () => {
     const response = await request(app).post('/register').send({
       name: 'John Doe',
       email: 'johndoe@mail.com',
-      cpf: '123456',
+      cpf: '81166815080',
       cellPhone: 999999,
     });
 
-    jest.spyOn(mockValidationCpf, 'isValid').mockReturnValueOnce(true);
-
     expect(response.status).toBe(201);
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('Should return 400 when there is a validation error', async () => {
+    const response = await request(app).post('/register').send({
+      name: 'John Doe',
+      email: 'johndoes@mail.com',
+      cpf: '8116681508',
+      cellPhone: 999999,
+    });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({
+      statusCode: 400,
+      message: 'Invalid CPF',
+    });
+  });
+
+  it('should return 400 when the email already exists', async () => {
+    const response = await request(app).post('/register').send({
+      name: 'John Doe',
+      email: 'johndoe@mail.com',
+      cpf: '81166815080',
+      cellPhone: 999999,
+    });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({
+      statusCode: 400,
+      message: 'Email already in use!',
+    });
   });
 });
