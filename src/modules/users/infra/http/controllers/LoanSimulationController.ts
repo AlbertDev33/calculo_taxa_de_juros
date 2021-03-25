7b557a5d972e1e0b466b4c696fd9db9276c19d8e
AlbertDev33 <alber725@gmail.com>
2021-03-25 15:54:02 -0300

    refactor: add try catch

--- src/modules/users/infra/http/controllers/LoanSimulationController.ts
+++ src/modules/users/infra/http/controllers/LoanSimulationController.ts
@@ -1,7 +1,10 @@
+import { AppError } from '../../../../../shared/errors/AppError';
+import { ClientRequestError } from '../../../../../shared/errors/ClientRequestError';
 import {
   IRequest,
   IResponse,
 } from '../../../../../shared/providers/ExpressProvider/HttpRequest';
+import { IExpressCreditSource } from '../../../useCases/LoanSimulation/LoanSimulationUseCase';
 import { ILoanSimulationUseCase } from '../../../useCases/LoanSimulation/model/ILoanSimulationUseCase';
 
 export class LoanSimulationController {
@@ -11,13 +14,43 @@ export class LoanSimulationController {
     const { email, score } = request.user;
     const { installments, value } = request.body;
 
-    const loanSimulation = await this.loanSimulationUseCase.execute({
-      email,
-      installments,
-      value,
-      score,
-    });
+    try {
+      const loanSimulation = await this.loanSimulationUseCase.execute({
+        email,
+        installments,
+        value,
+        score,
+      });
 
-    return response.status(201).json(loanSimulation);
+      return response.status(201).json(loanSimulation);
+    } catch (err) {
+      const installmentsMap = new Map([
+        [6, 6],
+        [12, 12],
+        [18, 18],
+        [24, 24],
+        [36, 36],
+      ]);
+
+      if (!installmentsMap.has(installments)) {
+        throw new AppError('Enter a valid installments', err.status);
+      }
+
+      await this.returnErrorInvalidParameters(err, {
+        installments,
+        value,
+      });
+
+      throw new ClientRequestError('Network Error!', err.status);
+    }
+  }
+
+  private async returnErrorInvalidParameters(
+    err: any,
+    { installments, value, findInterestRate }: IExpressCreditSource,
+  ): Promise<void> {
+    if (!installments || !value) {
+      throw new AppError(`${err.response.data}`, err.response.status);
+    }
   }
 }
