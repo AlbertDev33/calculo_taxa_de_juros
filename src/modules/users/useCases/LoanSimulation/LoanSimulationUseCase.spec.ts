14aec8fdbf1de6469d4be654a2273697a19f4fca
AlbertDev33 <alber725@gmail.com>
2021-03-26 15:30:03 -0300

    refactor: remove unused tests

--- src/modules/users/useCases/LoanSimulation/LoanSimulationUseCase.spec.ts
+++ src/modules/users/useCases/LoanSimulation/LoanSimulationUseCase.spec.ts
@@ -1,7 +1,6 @@
 /* eslint-disable max-classes-per-file */
 
 import { AppError } from '../../../../shared/errors/AppError';
-import { ClientRequestError } from '../../../../shared/errors/ClientRequestError';
 import { IRequestProvider } from '../../../../shared/providers/AxiosProvider/protocol/IRequestProvider';
 import {
   IRequestConfig,
@@ -49,21 +48,6 @@ const makeRequestProvider = (): IRequestProvider => {
 
 const makeRegisterAccountRepository = (): IRegisterAccountRepository => {
   class RegisterAccountStub implements IRegisterAccountRepository {
-    findByCpf(cpf: string): Promise<Account> {
-      const fakeAccount = {
-        id: 'valid_id',
-        name: 'John Doe',
-        email: 'any_email@mail.com',
-        cpf: 'hashed_cpf',
-        cellPhone: 555555,
-        score: 0,
-        negative: false,
-        installments: 6,
-        value: 1000,
-      };
-
-      return new Promise(resolve => resolve(fakeAccount));
-    }
     create(account: IRegisterAccountDTO): Promise<Account> {
       return new Promise(resolve => resolve({ ...account, id: 'valid_id' }));
     }
@@ -103,6 +87,13 @@ const makeInterestRateRepository = (): IInterestRateRepository => {
       type,
       installments,
       rate,
+    }: InterestRateDTO): Promise<Rate | undefined> {
+      return new Promise(resolve => resolve({} as Rate));
+    }
+
+    findDuplicatedData({
+      type,
+      installments,
     }: InterestRateDTO): Promise<Rate | undefined> {
       return new Promise(resolve => resolve({} as Rate));
     }
@@ -232,77 +223,6 @@ describe('Loan Simulation', () => {
     const loanSimulation = await sut.execute(fakeAccount);
 
     expect(loanSimulation).toEqual(mockedReturnValueAxios);
-  });
-
-  it('Should return a generic error from LoanSimulationUseCase service when the request fail before reaching the service', async () => {
-    const { sut, requestProviderStub } = makeSut();
-
-    const fakeAccount = {
-      email: 'any_email@mail.com',
-      cpf: 'hashed_cpf',
-      score: 550,
-      negative: false,
-      installments: 6,
-      value: 1000,
-    };
-
-    jest
-      .spyOn(requestProviderStub, 'post')
-      .mockRejectedValue({ message: 'Network Error' });
-
-    const loanSimulation = sut.execute(fakeAccount);
-
-    await expect(loanSimulation).rejects.toThrow(
-      'Unexpected error when trying to communicate to Credito Express: Network Error',
-    );
-  });
-
-  it('Should return a message error from LoanSimulationUseCase service when the invalid installments number', async () => {
-    const { sut, requestProviderStub } = makeSut();
-
-    const fakeAccount = {
-      email: 'any_email@mail.com',
-      cpf: 'hashed_cpf',
-      score: 550,
-      negative: false,
-      installments: 16,
-      value: 1000,
-    };
-
-    jest.spyOn(requestProviderStub, 'post').mockRejectedValue({
-      response: {
-        data: 'Número de parecelas é inválida',
-        status: 400,
-      },
-    });
-
-    const loanSimulation = sut.execute(fakeAccount);
-
-    await expect(loanSimulation).rejects.toBeInstanceOf(AppError);
-  });
-
-  it('Should return a message error from LoanSimulationUseCase service if any parameters are not informed', async () => {
-    const { sut, requestProviderStub } = makeSut();
-
-    const fakeAccount = {
-      email: 'any_email@mail.com',
-      cpf: 'hashed_cpf',
-      score: 550,
-      negative: false,
-      installments: 12,
-      value: 0,
-    };
-
-    jest.spyOn(requestProviderStub, 'post').mockRejectedValue({
-      response: {
-        data: 'Todos os parâmetros devem ser informados',
-        status: 400,
-      },
-    });
-
-    const loanSimulation = sut.execute(fakeAccount);
-
-    await expect(loanSimulation).rejects.toBeInstanceOf(AppError);
   });
 
   it('Should return a message error from LoanSimulationUseCase service if findInterestRate parameter are not returned', async () => {
