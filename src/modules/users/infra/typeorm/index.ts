68053655556f1bb9350227e8c1c3c04e9fa5e807
albert <alber725@gmail.com>
2021-04-08 14:27:28 -0300

    refactor: delete file

--- src/modules/users/infra/typeorm/index.ts
+++
@@ -1,8 +0,0 @@
-import { createConnections, Connection } from 'typeorm';
-
-const closeConnection = new Connection({ type: 'mongodb' });
-
-export const openConnection = async (): Promise<Connection[]> =>
-  createConnections();
-
-export const disconnect = (): Promise<void> => closeConnection.close();
